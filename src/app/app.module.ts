0150c36c4f835467d8ccc17ecfd4d7e07bacb03d
Mazkid21 <zmaraziti@yahoo.com>
2017-08-25 11:01:50 -0600

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UsersPageComponent } from './users-page/users-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SignUpComponent,
+    HomePageComponent,
+    UsersPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule
