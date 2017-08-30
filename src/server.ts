cf7d191766e5090ea696f8e583bedce8d38c782a
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-08-30 11:33:00 -0600

    remove favicon.ico

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,10 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { readFileSync } from 'fs';
 import { join } from 'path';
-
 import * as cookieParser from 'cookie-parser';
 import * as Auth0Strategy   from 'passport-auth0';
 import {session} from 'express-session';
-
 import * as passport from 'passport';
-
 import * as yokelRoutes from './config/index';
 
 // const home = require('./config/main');
