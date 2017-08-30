b140654b3813723866a848a50f3725196fe2d721
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-08-30 14:04:37 -0600

    help

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,34 +1,34 @@
-// var passport = require("passport");
-// import { db } from '../models';
-// var Comment = db.models.Comment;
-// var Location = db.models.Location;
-// var User = db.models.User;
+var passport = require("passport");
+import { db } from '../models';
+var Comment = db.models.Comment;
+var Location = db.models.Location;
+var User = db.models.User;
 
-// // POST /signup
-// function postSignup(request, response, next) {
-// 	//save a new user
-// 	let signupStrategy = passport.authenticate('local-signup',{
-// 		successRedirect:'/',
-// 		failureRedirect:'/signup',
-// 		failureFlash: true
-// 	});
+// POST /signup
+function postSignup(request, response, next) {
+	//save a new user
+	let signupStrategy = passport.authenticate('local-signup',{
+		successRedirect:'/',
+		failureRedirect:'/signup',
+		failureFlash: true
+	});
 
-// 	return signupStrategy(request, response, next);
-// }
+	return signupStrategy(request, response, next);
+}
 
-// // POST /login 
-// function postLogin(request, response, next) {
-//     var loginProperty = passport.authenticate('local-login', {
-//       successRedirect : '/',
-//       failureRedirect : '/login',
-//       failureFlash : true
-//     });
+// POST /login 
+function postLogin(request, response, next) {
+    var loginProperty = passport.authenticate('local-login', {
+      successRedirect : '/',
+      failureRedirect : '/login',
+      failureFlash : true
+    });
 
-//     return loginProperty(request, response, next);
-//  }
+    return loginProperty(request, response, next);
+ }
 
-// // GET /logout
-// function getLogout(request, response,next) {
-// 	request.logout();
-// 	response.redirect('/');
-// }
+// GET /logout
+function getLogout(request, response,next) {
+	request.logout();
+	response.redirect('/');
+}
