01d3aedd5cd180c9f9933070933a2986b0e973f4
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-08-30 11:49:07 -0600

    things2

--- src/config/user.js
+++ src/config/user.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const passport = require('passport');
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
-const router = express.Router();
+// const express = require('express');
+// const passport = require('passport');
+// const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+// const router = express.Router();
 
-/* GET user profile. */
-router.get('/', ensureLoggedIn, function(req, res, next) {
-  res.render('user', {
-    user: req.user
-  });
-});
+// /* GET user profile. */
+// router.get('/', ensureLoggedIn, function(req, res, next) {
+//   res.render('user', {
+//     user: req.user
+//   });
+// });
 
-module.exports = router;
+// module.exports = router;
