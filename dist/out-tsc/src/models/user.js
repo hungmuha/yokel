20efdcdd9a39bd68e569e3d20f05910494338c88
hungmuha <hung.soboss@gmail.com>
2017-08-31 06:00:33 -0600

    getting it to work

--- dist/out-tsc/src/models/user.js
+++ dist/out-tsc/src/models/user.js
@@ -1,9 +1,9 @@
 module.exports = function (sequelize, Sequelize) {
     var model = sequelize.define('user', {
-        // id: Sequelize.INTERGER,
+        id: Sequelize.INTEGER,
         username: Sequelize.STRING,
         email: Sequelize.STRING,
-        locationId: Sequelize.INTERGER,
+        locationId: Sequelize.INTEGER,
     });
     return model;
 };
