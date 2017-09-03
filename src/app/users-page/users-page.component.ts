bcf3027fc0c82c1ffd5e9d600b63f4fbcd0f1430
laurenwelborn <lulu.welborn@gmail.com>
2017-09-03 00:16:29 -0600

    fixed comment errors

--- src/app/users-page/users-page.component.ts
+++ src/app/users-page/users-page.component.ts
@@ -3,10 +3,7 @@ import { ActivatedRoute }   from '@angular/router';
 import { CommentsService } from './comments.service';
 import { NgModule } from '@angular/core';
 
-// - REMOVE WHEN CONNECTED TO BACKEND -
-class comment {
-  text: string;
-}
+
 
 @Component({
   selector: 'app-users-page',
@@ -17,44 +14,27 @@ class comment {
 export class UsersPageComponent implements OnInit {
 	oneLocation;
   updateText;
-  location = 1234;
   // - REMOVE WHEN CONNECTED TO BACKEND -
-  newComment: comment;
+  // newComment: comment;
   // - USE WHEN CONNECTED TO BACKEND -
   // Comment= <any>{};
 
-  constructor(
-  	private route: ActivatedRoute,
-  	private commentsService : CommentsService,
+  // constructor(
+  // 	private route: ActivatedRoute,
+  // 	private commentsService : CommentsService,
 
-  ) {this.newComment = new comment();
-  }
-
-  comment = [
-     {text:'placeholder comment'},
-     
-  ];
+  // ) {this.newComment = new comment();
+  // }
 
   ngOnInit() {
-  	this.route.params.forEach(param => {
-  		this.commentsService.getOneLocation(param.id)
-  		.subscribe(response=>{
-  			console.log(response.json());
-  			this.oneLocation = response.json();
-  		});
-  	});
+  	// this.route.params.forEach(param => {
+  	// 	this.commentsService.getOneLocation(param.id)
+  	// 	.subscribe(response=>{
+  	// 		console.log(response.json());
+  	// 		this.oneLocation = response.json();
+  	// 	});
+  	// });
   }  
-
-  // - REMOVE WHEN CONNECTED TO BACKEND -
-  onSubmit(text) {
-    if (this.newComment) {
-      var entry = {
-        'text': this.newComment.text,
-      };
-      console.log("hello from onSubmit");
-      this.comment.push(entry);
-    }
-  }
 
   // - USE WHEN CONNECTED TO BACKEND -
   // onSubmit(location,Comment) {
